6c8ab9aec9677412f664a142b6c06297d236a796
Thomas Perivolas <thomas.perivolas@gmail.com>
2025-10-24 00:52:35 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    // do stuffs 
+    // documentation for adafruit IO : https://github.com/methio/adafruitIO-class
+    // documentation for alwan color picker : https://github.com/sefianecho/alwan
+    // documentation for Name That Color (NTC) : https://chir.ag/projects/ntc/
+    
+
+});

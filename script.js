c92de663e25dae79e3e6e90b4a1f321817ff1936
nichola2266 <nichola2266@gmail.com>
2025-10-28 18:57:42 +0100

    CatCuddles_coding

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", () => {
+    const grid = document.getElementById("grid");
+    const gridSize = 72;
 
-    // do stuffs 
-    // documentation for adafruit IO : https://github.com/methio/adafruitIO-class
-    // documentation for alwan color picker : https://github.com/sefianecho/alwan
-    // documentation for Name That Color (NTC) : https://chir.ag/projects/ntc/
-    
+    // generate cells
+    for(let i = 1; i <= gridSize; i++){
+        grid.innerHTML += `
+            <div id="cell-${i}" data-state="off" class="cell"></div>
+        `;
+    }
 
+    grid.addEventListener("click", (e) => {
+        const selectedCell = e.target;
+        selectedCell.dataset.state = selectedCell.dataset.state === "off" ? "on" : "off";
+        // console.log(target);
+
+    })
 });
